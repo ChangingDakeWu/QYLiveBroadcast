6b358b85ce7d315210164073303a72789f0937db
dake <1012506333@qq.com>
2016-10-31 10:32:10 +0800

    修改了观众观看底部按钮的功能

---
+++ app/src/main/java/com/edu/gdqy/Controller/LiveWatcherView/adapter/MyAdapter.java
@@ -0,0 +1,31 @@
+package com.edu.gdqy.Controller.LiveWatcherView.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by WDK on 2016/10/31.
+ */
+
+public class MyAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
